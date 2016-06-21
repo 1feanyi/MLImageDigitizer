74f993d77d885b3f6bf54b6f631aa0328da66c03
1feanyi <ifeanyi.igbo@gmail.com>
2016-06-20 22:26:59 -0400

    added distance and classifier

--- MLImageDigitizer/Program.cs
+++ MLImageDigitizer/Program.cs
@@ -11,7 +11,7 @@ namespace MLImageDigitizer
     {
         static void Main(string[] args)
         {
-            var FilePath = @"C:\Users\ifeanyi\Documents\visual studio 2013\Projects\MLImageDigitizer\MLImageDigitizer\Data\sampledata.csv";
+            var FilePath = @"C:\Users\ifeanyi\Documents\visual studio 2013\Projects\MLImageDigitizer\MLImageDigitizer\Data\trainingdata.csv";
             var reading = DataReader.ReadObservations(FilePath);
 
             Console.ReadLine();
@@ -48,6 +48,73 @@ namespace MLImageDigitizer
                 var data = File.ReadAllLines(dataPath).Skip(1).Select(ObservationFactory).ToArray();
             
                 return data;
+            }
+        }
+
+        public interface IDistance
+        {
+            double Between(int[] pixels1, int[] pixels2);
+        }
+
+        public class ManhattanDistance : IDistance
+        {
+            public double Between(int[] pixels1, int[] pixels2)
+            {
+                if (pixels1.Length != pixels2.Length)
+                {
+                    throw new ArgumentException("Inconsistent image sizes.");
+                }
+
+                var length = pixels1.Length;
+
+                var distance = 0;
+
+                for (int i = 0; i < length; i++)
+                {
+                    distance += Math.Abs(pixels1[i] - pixels2[i]);
+                }
+                
+                return distance;
+            }
+        }
+
+        public interface IClassifier
+        {
+            void Train(IEnumerable<Observation> trainingSet);
+            string Predict(int[] pixels);
+        }
+
+        public class BasicClassifier : IClassifier
+        {
+            private IEnumerable<Observation> data;
+            private readonly IDistance distance;
+
+            public BasicClassifier(IDistance distance)
+            {
+                this.distance = distance;
+            }
+
+            public void Train(IEnumerable<Observation> trainingSet)
+            {
+                this.data = trainingSet;
+            }
+
+            public string Predict(int[] pixels)
+            {
+                Observation currentBest = null;
+                var shortest = Double.MaxValue;
+
+                foreach (Observation obs in this.data)
+                {
+                    var dist = this.distance.Between(obs.Pixels, pixels);
+                    if (dist < shortest)
+                    {
+                        shortest = dist;
+                        currentBest = obs;
+                    }
+                }
+
+                return currentBest.Label;
             }
         }
 
